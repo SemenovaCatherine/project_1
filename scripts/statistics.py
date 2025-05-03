63199bbae5068f1b056d284d861f0576e2d6ff2e
SemenovaCatherine <semenovkata781@gmail.com>
2025-05-03 22:48:57 +0300

    Update statistics.py

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def compute_statistics(df):
+    if df.empty:
+        raise ValueError("DataFrame пуст. Проверьте загруженные данные.")
+
     stats = df.groupby('city').agg({
         'temperature': ['mean', 'min', 'max'],
         'rain': 'sum'
     })
     stats.columns = ['avg_temp', 'min_temp', 'max_temp', 'total_rain']
     return stats.reset_index()
-    
-if df.empty:
-    raise ValueError("DataFrame пуст. Проверьте загруженные данные.")
